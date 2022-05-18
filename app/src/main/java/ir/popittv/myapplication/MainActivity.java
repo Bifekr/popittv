73e3613bf71be4a0a50e1ae018d1df52a87bb937
miladjamshidpur <miladjamshidpur@gmail.com>
2022-05-18 13:51:25 +0430

    finish tab-vp2

--- app/src/main/java/ir/popittv/myapplication/MainActivity.java
+++ app/src/main/java/ir/popittv/myapplication/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     FragmentManager manager;
     FragmentMain1 fragmentMain1;
     private ActivityMainBinding binding;
+    View view1;
 
 
     @Override
@@ -35,14 +36,45 @@ public class MainActivity extends AppCompatActivity {
         transaction = getSupportFragmentManager().beginTransaction();
 
 
-    binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setIcon(R.drawable.iv_btn_1_border));
-        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setIcon(R.drawable.iv_btn_2));
+        view1=getLayoutInflater().inflate(R.layout.custom_tab,null,false);
+        view1.findViewById(R.id.icon).setBackgroundResource(R.drawable.iv_btn_1);
+        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setCustomView(view1));
+
+        View view2=getLayoutInflater().inflate(R.layout.custom_tab,null,false);
+        view2.findViewById(R.id.icon).setBackgroundResource(R.drawable.iv_btn_2);
+        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setCustomView(view2));
+
+        View view3=getLayoutInflater().inflate(R.layout.custom_tab,null,false);
+        view3.findViewById(R.id.icon).setBackgroundResource(R.drawable.iv_btn_3);
+        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setCustomView(view3));
+
+        View view4=getLayoutInflater().inflate(R.layout.custom_tab,null,false);
+        view4.findViewById(R.id.icon).setBackgroundResource(R.drawable.iv_btn_4);
+        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setCustomView(view4));
+
+        View view5=getLayoutInflater().inflate(R.layout.custom_tab,null,false);
+        view5.findViewById(R.id.icon).setBackgroundResource(R.drawable.iv_btn_5);
+        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setCustomView(view5));
+
+        View view6=getLayoutInflater().inflate(R.layout.custom_tab,null,false);
+        view6.findViewById(R.id.icon).setBackgroundResource(R.drawable.iv_btn_6);
+        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setCustomView(view6));
+
+        View view7=getLayoutInflater().inflate(R.layout.custom_tab,null,false);
+        view7.findViewById(R.id.icon).setBackgroundResource(R.drawable.iv_btn_7);
+        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setCustomView(view7));
+
+
+
+
+
+        /*binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setIcon(R.drawable.iv_btn_2));
         binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setIcon(R.drawable.iv_btn_3));
         binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setIcon(R.drawable.iv_btn_4));
         binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setIcon(R.drawable.iv_btn_5));
         binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setIcon(R.drawable.iv_btn_6));
-        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setIcon(R.drawable.iv_btn_7));
-        binding.tblMainActivity.setTabGravity(TabLayout.GRAVITY_CENTER);
+        binding.tblMainActivity.addTab(binding.tblMainActivity.newTab().setIcon(R.drawable.iv_btn_7));*/
+
 
         int numTab = binding.tblMainActivity.getTabCount();
 
@@ -52,33 +84,35 @@ public class MainActivity extends AppCompatActivity {
             public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
 
 
-
-                switch (position){
+                switch (position) {
                     case 0:
-                        tab.setIcon(R.drawable.iv_btn_1);
+                        tab.setCustomView(view1);
                         break;
                     case 1:
-                        tab.setIcon(R.drawable.iv_btn_2);
+
+                        tab.setCustomView(view2);
                         break;
                     case 2:
-                        tab.setIcon(R.drawable.iv_btn_3);
+
+                        tab.setCustomView(view3);
                         break;
                     case 3:
-                        tab.setIcon(R.drawable.iv_btn_4);
+
+                        tab.setCustomView(view4);
                         break;
                     case 4:
-                        tab.setIcon(R.drawable.iv_btn_5);
+
+                        tab.setCustomView(view5);
                         break;
                     case 5:
-                        tab.setIcon(R.drawable.iv_btn_6);
+
+                        tab.setCustomView(view6);
                         break;
                     case 6:
-                        tab.setIcon(R.drawable.iv_btn_7);
+                        tab.setCustomView(view7);
                         break;
 
                 }
-
-
 
 
             }
