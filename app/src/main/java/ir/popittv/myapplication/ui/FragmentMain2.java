4997437b21a60395e3c86483b0a691abd16908a7
miladjamshidpur <miladjamshidpur@gmail.com>
2022-06-08 02:47:44 +0430

    clean viewPager2 2

--- app/src/main/java/ir/popittv/myapplication/ui/FragmentMain2.java
+++ app/src/main/java/ir/popittv/myapplication/ui/FragmentMain2.java
@@ -5,23 +5,25 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import java.util.List;
 
-import ir.popittv.myapplication.adapter.Frg2_VpSelect_Adapter;
+import ir.popittv.myapplication.adapter.Frg1Rv1_Adapter;
 import ir.popittv.myapplication.databinding.FragmentMain2Binding;
 import ir.popittv.myapplication.models.FunnyDataModel;
 import ir.popittv.myapplication.viewmodel.MainViewModel;
 
 public class FragmentMain2 extends Fragment {
 
-    Frg2_VpSelect_Adapter select_adapter;
+    Frg1Rv1_Adapter adapter;
     private FragmentMain2Binding binding;
     private MainViewModel mainViewModel;
     private Handler slideHandler = new Handler();
@@ -44,6 +46,13 @@ public class FragmentMain2 extends Fragment {
 
         //set data into dataModel
         mainViewModel.requestFunny_best();
+
+        // RecyclerView 1
+       adapter = new Frg1Rv1_Adapter(getActivity());
+        binding.rv4Frg1.setAdapter(adapter);
+        binding.rv4Frg1.setLayoutManager(new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.HORIZONTAL, false));
+
         //get data from dataModel
         getBestFunny();
 
@@ -57,8 +66,11 @@ public class FragmentMain2 extends Fragment {
             @Override
             public void onChanged(List<FunnyDataModel> funnyDataModels) {
                 if (funnyDataModels!=null) {
-                    select_adapter.setFunnyDataModelList(funnyDataModels);
-
+                    adapter.setData(funnyDataModels);
+                    for (FunnyDataModel cafe:funnyDataModels
+                    ) {
+                        Toast.makeText(getContext(), "+++" + cafe.getPoster(), Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
