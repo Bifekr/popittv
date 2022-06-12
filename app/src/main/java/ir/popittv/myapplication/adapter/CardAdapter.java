14a3aa31d8a28668170d39db612692cb43051583
miladjamshidpur <miladjamshidpur@gmail.com>
2022-06-13 00:34:36 +0430

    item_sweep_viewpager.xml

--- app/src/main/java/ir/popittv/myapplication/adapter/CardAdapter.java
+++ app/src/main/java/ir/popittv/myapplication/adapter/CardAdapter.java
@@ -4,7 +4,7 @@ import androidx.cardview.widget.CardView;
 
 public interface CardAdapter {
 
-    int MAX_ELEVATION_FACTOR = 15;
+    int MAX_ELEVATION_FACTOR = 10;
 
     float getBaseElevation();
     CardView getCardViewAt(int position);
