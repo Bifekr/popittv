7a3c71c02ad630114b9a267c2466f178f96c6ff7
miladjamshidpur <miladjamshidpur@gmail.com>
2022-06-21 05:08:04 +0430

    change frg1 into Activity

--- app/src/main/java/ir/popittv/myapplication/MainActivity.java
+++ app/src/main/java/ir/popittv/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package ir.popittv.myapplication;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,17 +13,34 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ir.popittv.myapplication.adapter.CardPagerAdapter2;
+import ir.popittv.myapplication.adapter.ChannelDetail_adapter;
+import ir.popittv.myapplication.adapter.InfinitFrg1_PagerAdapter;
+import ir.popittv.myapplication.adapter.Recommend_Adapter;
+import ir.popittv.myapplication.adapter.RvChannel_Frg1;
 import ir.popittv.myapplication.databinding.ActivityMainBinding;
+import ir.popittv.myapplication.databinding.FragmentMain1Binding;
+import ir.popittv.myapplication.models.ChannelDataModel;
+import ir.popittv.myapplication.models.FunnyDataModel;
 import ir.popittv.myapplication.ui.FragmentMain1;
 import ir.popittv.myapplication.ui.FragmentMain2;
 import ir.popittv.myapplication.ui.FragmentMain3;
 import ir.popittv.myapplication.ui.FragmentMain4;
+import ir.popittv.myapplication.ui.RealityActivity;
+import ir.popittv.myapplication.ui.TestActivity;
+import ir.popittv.myapplication.utils.OnClickFrg1;
 import ir.popittv.myapplication.viewmodel.MainViewModel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickFrg1 {
 
     MainViewModel mainViewModel;
     ActivityMainBinding binding;
@@ -30,6 +48,27 @@ public class MainActivity extends AppCompatActivity {
     FragmentTransaction transaction;
     Fragment fragment;
     boolean back = true;
+
+
+
+
+    //global adapter
+    private RvChannel_Frg1 rvChannel_frg1;
+    private ChannelDetail_adapter detail_adapter;
+    private InfinitFrg1_PagerAdapter infinitAdapter;
+    Recommend_Adapter recommend_adapter;
+
+
+    //global Variable
+    int id_channel;
+    String tv1;
+
+
+    //Utils Class
+    private CardPagerAdapter2 cardPagerAdapter2;
+    private ShadowTransformer shadowTransformer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +79,175 @@ public class MainActivity extends AppCompatActivity {
 
         //ViewModel Provider
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        binding.navRail.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.schedule:
+                        startActivity(new Intent(MainActivity.this, TestActivity.class));
+                        break;
+                    case R.id.timer:
+                        startActivity(new Intent(MainActivity.this, RealityActivity.class));
+                        break;
 
-       if (fragment==null) {
+
+                }
+            return true;
+            }
+        });
+       /* binding.navRail.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.schedule:
+                        startActivity(new Intent(MainActivity.this,TestActivity.class));
+                        break;
+                    case R.id.timer:
+                        startActivity(new Intent(MainActivity.this, RealityActivity.class));
+                        break;
+
+
+
+                }
+            }
+        });*/
+
+  /*     if (fragment==null) {
            fragmentManager = getSupportFragmentManager();
            transaction = fragmentManager.beginTransaction();
            transaction.replace(binding.containerMain.getId(), new FragmentMain1()).commit();
        }
-        initRail();
+        initRail();*/
+
+
+
+        //request from Api to get DataModel
+        mainViewModel.requestChannel();
+        //detail Channel Selected
+        mainViewModel.requestChannel_detail(3);
+        // mainViewModel.requestChannel_detail(1);
+        mainViewModel.requestFunny_view();
+
+
+        initRv_Vp_adapter();
+
+
+        //update AND get Data from DataModel into LiveData
+        getChannel();
+        getChannel_detail();
+        getFunny_view();
+
+
+
+
 
     }
+
+
+
+    private void initRv_Vp_adapter() {
+
+        //init channel list Adapter
+        rvChannel_frg1 = new RvChannel_Frg1(this,this);
+        binding.rvChannelListFrg1.setAdapter(rvChannel_frg1);
+        binding.rvChannelListFrg1.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL, false));
+
+        //Show Detail Channel Recycler
+        detail_adapter=new ChannelDetail_adapter(this);
+        binding.rvDetailFrg1.setAdapter(detail_adapter);
+        binding.rvDetailFrg1.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL,false));
+
+
+        //horizontal viewpager infinite
+        infinitAdapter = new InfinitFrg1_PagerAdapter(this);
+        binding.infinitCycleFrg1.setAdapter(infinitAdapter);
+
+        //Recommended Vide Rv
+        recommend_adapter=new Recommend_Adapter(this);
+        binding.rvRecommendFrg1.setLayoutManager(new LinearLayoutManager(this,
+                RecyclerView.VERTICAL,false));
+        binding.rvRecommendFrg1.setAdapter(recommend_adapter);
+
+
+
+     /*   cardPagerAdapter2 = new CardPagerAdapter2(getActivity());
+        shadowTransformer = new ShadowTransformer(binding.vpChannelListFrg1, cardPagerAdapter2);
+        binding.vpChannelListFrg1.setAdapter(cardPagerAdapter2);
+        binding.vpChannelListFrg1.setPageTransformer(false, shadowTransformer);
+        binding.vpChannelListFrg1.setOffscreenPageLimit(6);*/
+
+
+    }
+
+    private void getChannel() {
+        mainViewModel.getChannel().observe(this,channelDataModels -> {
+            if (channelDataModels!=null) {
+                rvChannel_frg1.setData(channelDataModels);
+
+                for (ChannelDataModel channel : channelDataModels
+                ) {
+//                    cardPagerAdapter2.addCardItem(channel);
+
+                }
+            }
+        });
+    }
+    private void getChannel_detail(){
+        mainViewModel.getChannel_detail().observe(this, channelDataModel -> {
+
+
+
+
+            Glide.with(this).load(channelDataModel.getProfile_chann())
+                    .into(binding.showProfileChanFrg1);
+
+            List<FunnyDataModel> models = new ArrayList<>((channelDataModel).getVideos_channel());
+            detail_adapter.setFunnyDataModels(models);
+
+
+            //  binding.tvSubDetailChanFrg1.setText(tv1);
+            // binding.tvAgeDetailChanFrg1.setText(channelDataModel.getAge()+"");
+            // binding.titleFaDetailChanFrg1.setText(channelDataModel.getName_chan_fa()+"");
+            // binding.titleEnDetailChanFrg1.setText(channelDataModel.getName_chan_en()+"");
+
+
+
+        });
+    }
+
+    private void getFunny_view() {
+        mainViewModel.getFunny_view().observe(this, funnyDataModels -> {
+            if (funnyDataModels!=null) {
+                infinitAdapter.setData(funnyDataModels);
+                recommend_adapter.setFunnyDataModels(funnyDataModels);
+               /* for (FunnyDataModel funnyDataModel : funnyDataModels) {
+                     Toast.makeText(getActivity(), "" + funnyDataModel.getTitle_en(), Toast.LENGTH_SHORT).show();
+                }*/
+            } else {
+                Toast.makeText(this, "ggggg", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public void OnclickDetail(int pos) {
+
+        id_channel=pos;
+        mainViewModel.requestChannel_detail(id_channel);
+
+    }
+
+
+
+
+
+
+
 
 
     private void initRail() {
