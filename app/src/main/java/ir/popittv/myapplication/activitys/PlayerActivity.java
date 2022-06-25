d5152f061595bbbb6c898720a13a1a16d1c35888
miladjamshidpur <miladjamshidpur@gmail.com>
2022-06-25 08:38:45 +0430

    transition pair detail activity

---
+++ app/src/main/java/ir/popittv/myapplication/activitys/PlayerActivity.java
@@ -0,0 +1,21 @@
+package ir.popittv.myapplication.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.popittv.myapplication.R;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    private int id_vid_funny;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+
+
+    }
+}
