cbd03c00e7c02c53846ada0bf62ee0a19df58e53
miladjamshidpur <miladjamshidpur@gmail.com>
2022-06-25 09:36:26 +0430

    transition pair detail activity

--- app/src/main/java/ir/popittv/myapplication/activitys/PlayerActivity.java
+++ app/src/main/java/ir/popittv/myapplication/activitys/PlayerActivity.java
@@ -1,19 +1,30 @@
 package ir.popittv.myapplication.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
 
 import ir.popittv.myapplication.R;
+import ir.popittv.myapplication.databinding.ActivityPlayerBinding;
+import ir.popittv.myapplication.viewmodel.MainViewModel;
 
 public class PlayerActivity extends AppCompatActivity {
 
+    private ActivityPlayerBinding binding;
+    private MainViewModel viewModel;
+
     private int id_vid_funny;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+        binding=ActivityPlayerBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
+        viewModel=new ViewModelProvider(this).get(MainViewModel.class);
 
 
 
