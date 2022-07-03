9abeb4a6800065da368963720f323ec296a95d5f
miladjamshidpur <miladjamshidpur@gmail.com>
2022-07-03 20:21:16 +0430

    login Activity

--- app/src/main/java/ir/popittv/myapplication/activitys/UserActivity.java
+++ app/src/main/java/ir/popittv/myapplication/activitys/UserActivity.java
@@ -56,130 +56,141 @@ public class UserActivity extends AppCompatActivity {
 
         binding.avatarUserUserActivity.setOnClickListener(v -> {
 
-            String check = sharedPreferences.getString("name_user", "");
-            if (check.equals("")) {
-
-                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
-                bottomView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
-                bottomSheetDialog.setContentView(bottomView);
-                BottomSheetBehavior sheetBehavior = BottomSheetBehavior.from((View) bottomView.getParent());
-                sheetBehavior.setPeekHeight(
-
-                        (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 500, getResources().getDisplayMetrics())
-                );
-
-
-                bottomSheetDialog.show();
-                et_phone = bottomView.findViewById(R.id.et_phone_userProfile);
-                Button btn_send = bottomView.findViewById(R.id.send_customLogin);
-                ProgressBar progressBar = bottomView.findViewById(R.id.progress_dialog);
-
-
-                btn_send.setOnClickListener(V -> {
-
-                    if (et_phone.getEditText().getText().toString().equals("")) {
-                        et_phone.setError("شماره را وارد کنید");
-
-                    } else {
-
-                        progressBar.setVisibility(View.VISIBLE);
-                        phone_user = et_phone.getEditText().getText().toString();
-                        editor = sharedPreferences.edit();
-                        editor.putString("phone_user", phone_user);
-                        editor.apply();
-                        Service.getApiClient().userLogin(phone_user).enqueue(new Callback<ResponseBody>() {
-                            @Override
-                            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-
-                                bottomSheetDialog.dismiss();
-
-                                BottomSheetDialog bottomSheetDialog2 = new BottomSheetDialog(UserActivity.this);
-                                bottomView2 = getLayoutInflater().inflate(R.layout.coustom_dialog_code, null);
-                                bottomSheetDialog2.setContentView(bottomView2);
-                                bottomSheetDialog2.show();
-
-                                TextInputLayout et_code = bottomView2.findViewById(R.id.et_code_userProfile);
-                                Button btn_code = bottomView2.findViewById(R.id.sendCode_customLogin);
-                                btn_code.setOnClickListener(v1 -> {
-
-                                    if (et_code.getEditText().getText().toString().equals("")) {
-                                        et_code.setError("کد را وارد کنید");
-
-                                    } else {
-                                        code_user = et_code.getEditText().getText().toString();
-                                        Service.getApiClient().getUser(phone_user, code_user).enqueue(new Callback<UserDataModel>() {
-                                            @Override
-                                            public void onResponse(Call<UserDataModel> call1, Response<UserDataModel> response1) {
-                                                UserDataModel userDataModel = response1.body();
-                                                if (response1.isSuccessful()) {
-                                                    assert userDataModel!=null;
-                                                    id_user = userDataModel.getUser_id();
-                                                    name_user = userDataModel.getName();
-                                                    editor = sharedPreferences.edit();
-                                                    editor.putString("name_user", name_user);
-                                                    editor.putInt("id_user", id_user);
-                                                    editor.apply();
-                                                    bottomSheetDialog2.dismiss();
-                                                    binding.phoneNumUserActivity.setText(phone_user);
-                                                    binding.userNameUserActivity.setText(name_user);
-                                                    binding.avatarUserUserActivity.setBackgroundResource(R.drawable.trophy);
-
-                                                }
-
-                                            }
-
-                                            @Override
-                                            public void onFailure(Call<UserDataModel> call1, Throwable t) {
-                                                Toast.makeText(UserActivity.this, "wrong", Toast.LENGTH_LONG).show();
-                                            }
-                                        });
-                                    }
-                                });
-
-
-                            }
-
-                            @Override
-                            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                                Toast.makeText(UserActivity.this, "send again", Toast.LENGTH_SHORT).show();
-
-                            }
-                        });
-
-                    }
-
-
-                });
-            } else {
-                String news=sharedPreferences.getString("phone_user","");
-                Toast.makeText(this, "قبلا وارد شده اید"+news, Toast.LENGTH_SHORT).show();
-            }
+            loginUser();
 
 
         });
 
-        String news=sharedPreferences.getString("phone_user",null);
-        String nammm=sharedPreferences.getString("name_user",null);
-        if (news!=null){
+        String news = sharedPreferences.getString("phone_user", null);
+        String nammm = sharedPreferences.getString("name_user", null);
+        if (news!=null) {
             binding.phoneNumUserActivity.setText(news);
             binding.userNameUserActivity.setText(nammm);
             binding.avatarUserUserActivity.setBackgroundResource(R.drawable.trophy);
+            binding.tvExitUserActivity.setText(R.string.exite);
 
-        }else {
+        } else {
             binding.phoneNumUserActivity.setText(R.string.hint_number);
             binding.userNameUserActivity.setText(R.string.hint_user_name);
-            binding.avatarUserUserActivity.setBackgroundResource(R.drawable.ic_baseline_exit_to_app_24);
+            binding.tvExitUserActivity.setText(R.string.enter);
+
         }
 
-        binding.tvExitUserActivity.
+        binding.tvExitUserActivity.setOnClickListener(v -> {
 
-        loginProfile();
+            sharedPreferences = this.getSharedPreferences("user_info", MODE_PRIVATE);
+            SharedPreferences.Editor editor22 = sharedPreferences.edit();
+            editor22.putString("phone_user", null);
+            editor22.apply();
+            Toast.makeText(getApplicationContext(), "شما از حساب خود خارج شدید", Toast.LENGTH_SHORT).show();
+
+
+        });
 
 
     }
 
-    private void loginProfile() {
 
+    private void loginUser() {
+        String check = sharedPreferences.getString("phone_user", "");
+        if (check.equals("")) {
+
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+            bottomView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
+            bottomSheetDialog.setContentView(bottomView);
+            BottomSheetBehavior sheetBehavior = BottomSheetBehavior.from((View) bottomView.getParent());
+            sheetBehavior.setPeekHeight(
+
+                    (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 500, getResources().getDisplayMetrics())
+            );
+
+
+            bottomSheetDialog.show();
+            et_phone = bottomView.findViewById(R.id.et_phone_userProfile);
+            Button btn_send = bottomView.findViewById(R.id.send_customLogin);
+            ProgressBar progressBar = bottomView.findViewById(R.id.progress_dialog);
+
+
+            btn_send.setOnClickListener(V -> {
+
+                if (et_phone.getEditText().getText().toString().equals("")) {
+                    et_phone.setError("شماره را وارد کنید");
+
+                } else {
+
+                    progressBar.setVisibility(View.VISIBLE);
+                    phone_user = et_phone.getEditText().getText().toString();
+                    editor = sharedPreferences.edit();
+                    editor.putString("phone_user", phone_user);
+                    editor.apply();
+                    Service.getApiClient().userLogin(phone_user).enqueue(new Callback<ResponseBody>() {
+                        @Override
+                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                            bottomSheetDialog.dismiss();
+
+                            BottomSheetDialog bottomSheetDialog2 = new BottomSheetDialog(UserActivity.this);
+                            bottomView2 = getLayoutInflater().inflate(R.layout.coustom_dialog_code, null);
+                            bottomSheetDialog2.setContentView(bottomView2);
+                            bottomSheetDialog2.show();
+
+                            TextInputLayout et_code = bottomView2.findViewById(R.id.et_code_userProfile);
+                            Button btn_code = bottomView2.findViewById(R.id.sendCode_customLogin);
+                            btn_code.setOnClickListener(v1 -> {
+
+                                if (et_code.getEditText().getText().toString().equals("")) {
+                                    et_code.setError("کد را وارد کنید");
+
+                                } else {
+                                    code_user = et_code.getEditText().getText().toString();
+                                    Service.getApiClient().getUser(phone_user, code_user).enqueue(new Callback<UserDataModel>() {
+                                        @Override
+                                        public void onResponse(Call<UserDataModel> call1, Response<UserDataModel> response1) {
+                                            UserDataModel userDataModel = response1.body();
+                                            if (response1.isSuccessful()) {
+                                                assert userDataModel!=null;
+                                                id_user = userDataModel.getUser_id();
+                                                name_user = userDataModel.getName();
+                                                editor = sharedPreferences.edit();
+                                                editor.putString("name_user", name_user);
+                                                editor.putInt("id_user", id_user);
+                                                editor.apply();
+                                                bottomSheetDialog2.dismiss();
+                                                binding.phoneNumUserActivity.setText(phone_user);
+                                                binding.userNameUserActivity.setText(name_user);
+                                                binding.avatarUserUserActivity.setBackgroundResource(R.drawable.trophy);
+                                                binding.tvExitUserActivity.setText(R.string.exite);
+
+                                            }
+
+                                        }
+
+                                        @Override
+                                        public void onFailure(Call<UserDataModel> call1, Throwable t) {
+                                            Toast.makeText(UserActivity.this, "wrong", Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+                                }
+                            });
+
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponseBody> call, Throwable t) {
+                            Toast.makeText(UserActivity.this, "send again", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
+
+
+            });
+        } else {
+            String news = sharedPreferences.getString("phone_user", "");
+            Toast.makeText(this, "قبلا وارد شده اید" + news, Toast.LENGTH_SHORT).show();
+        }
 
     }
 
