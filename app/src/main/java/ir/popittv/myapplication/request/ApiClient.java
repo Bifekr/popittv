cffe14134395b07d5587639e13dac87799ff9e52
miladjamshidpur <miladjamshidpur@gmail.com>
2022-07-06 03:21:22 +0430

    login Activity

--- app/src/main/java/ir/popittv/myapplication/request/ApiClient.java
+++ app/src/main/java/ir/popittv/myapplication/request/ApiClient.java
@@ -5,12 +5,10 @@ import ir.popittv.myapplication.models.FunnyDataModel;
 import ir.popittv.myapplication.models.UserDataModel;
 import ir.popittv.myapplication.response.ChannelResponse;
 import ir.popittv.myapplication.response.FunnyResponse;
+import ir.popittv.myapplication.response.UserResponse;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ApiClient {
@@ -18,29 +16,36 @@ public interface ApiClient {
 
     @GET("getFunny.php")
     Call<FunnyResponse> getFunny(@Query("id_subMenu") int id_subMenu);
+
     @GET("getFunny_single.php")
     Call<FunnyDataModel> getFunny_single(@Query("id_funny") int id_funny);
+
     @GET("getFunny_best.php")
     Call<FunnyResponse> getFunny_best();
+
     @GET("getFunny_liky.php")
     Call<FunnyResponse> getFunny_liky();
+
     @GET("getFunny_view.php")
     Call<FunnyResponse> getFunny_view();
 
     @GET("getChannel.php")
     Call<ChannelResponse> getChannel_kind(@Query("kind") int kind);
+
     @GET("getChannel_detail.php")
     Call<ChannelDataModel> getChannel_detail(@Query("id_channel") int id_channel);
-    @GET("getChannel_all.php")
-    Call<ChannelResponse> getChannel_all(@Query("kind") int kind , @Query("age") int age);
 
+    @GET("getChannel_all.php")
+    Call<ChannelResponse> getChannel_all(@Query("kind") int kind, @Query("age") int age);
 
 
     @GET("login.php")
     Call<ResponseBody> userLogin(@Query("phone") String phone);
 
-
     @GET("getCode.php")
-    Call<UserDataModel> getUser(@Query("phone") String phone,@Query("code") String code);
+    Call<UserDataModel> getUser(@Query("phone") String phone, @Query("code") String code);
+
+    @GET("getUser_sub.php")
+    Call<UserResponse> getUser_sub(@Query("id_user") String id_user, @Query("kind") int kind);
 
 }
