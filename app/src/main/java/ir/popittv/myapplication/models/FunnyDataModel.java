0d03486605de1ce1dd5c968ef4d6ff4c4c912759
miladjamshidpur <miladjamshidpur@gmail.com>
2022-07-06 23:20:42 +0430

    login Activity

--- app/src/main/java/ir/popittv/myapplication/models/FunnyDataModel.java
+++ app/src/main/java/ir/popittv/myapplication/models/FunnyDataModel.java
@@ -7,6 +7,9 @@ public class FunnyDataModel implements Parcelable {
 
     private int id_funny;
     private int id_channel;
+    private int id_reality;
+    private int id_study;
+    private int id_farsi;
     private String view;
     private String liky;
     private String title_en;
@@ -14,38 +17,19 @@ public class FunnyDataModel implements Parcelable {
     private String poster;
     private String link_480;
     private String link_720;
-    private String name_chan_en;
-    private String name_chan_fa;
     private String profile_chann;
     private String banner_chann;
     private String followers;
+    private String age_name;
+    private int kind;
 
-    public FunnyDataModel() {
-    }
-
-    public FunnyDataModel(int id_funny, int id_channel, String view, String liky, String title_en,
-                          String title_fa, String poster, String link_480, String link_720,
-                          String name_chan_en, String name_chan_fa, String profile_chann,
-                          String banner_chann, String followers) {
-        this.id_funny = id_funny;
-        this.id_channel = id_channel;
-        this.view = view;
-        this.liky = liky;
-        this.title_en = title_en;
-        this.title_fa = title_fa;
-        this.poster = poster;
-        this.link_480 = link_480;
-        this.link_720 = link_720;
-        this.name_chan_en = name_chan_en;
-        this.name_chan_fa = name_chan_fa;
-        this.profile_chann = profile_chann;
-        this.banner_chann = banner_chann;
-        this.followers = followers;
-    }
 
     protected FunnyDataModel(Parcel in) {
         id_funny = in.readInt();
         id_channel = in.readInt();
+        id_reality = in.readInt();
+        id_study = in.readInt();
+        id_farsi = in.readInt();
         view = in.readString();
         liky = in.readString();
         title_en = in.readString();
@@ -53,11 +37,11 @@ public class FunnyDataModel implements Parcelable {
         poster = in.readString();
         link_480 = in.readString();
         link_720 = in.readString();
-        name_chan_en = in.readString();
-        name_chan_fa = in.readString();
         profile_chann = in.readString();
         banner_chann = in.readString();
         followers = in.readString();
+        age_name = in.readString();
+        kind = in.readInt();
     }
 
     public static final Creator<FunnyDataModel> CREATOR = new Creator<FunnyDataModel>() {
@@ -78,6 +62,18 @@ public class FunnyDataModel implements Parcelable {
 
     public int getId_channel() {
         return id_channel;
+    }
+
+    public int getId_reality() {
+        return id_reality;
+    }
+
+    public int getId_study() {
+        return id_study;
+    }
+
+    public int getId_farsi() {
+        return id_farsi;
     }
 
     public String getView() {
@@ -108,14 +104,6 @@ public class FunnyDataModel implements Parcelable {
         return link_720;
     }
 
-    public String getName_chan_en() {
-        return name_chan_en;
-    }
-
-    public String getName_chan_fa() {
-        return name_chan_fa;
-    }
-
     public String getProfile_chann() {
         return profile_chann;
     }
@@ -128,6 +116,41 @@ public class FunnyDataModel implements Parcelable {
         return followers;
     }
 
+    public String getAge_name() {
+        return age_name;
+    }
+
+    public int getKind() {
+        return kind;
+    }
+
+    public FunnyDataModel() {
+    }
+
+    public FunnyDataModel(int id_funny, int id_channel, int id_reality, int id_study,
+                          int id_farsi, String view, String liky, String title_en,
+                          String title_fa, String poster, String link_480, String link_720,
+                          String profile_chann, String banner_chann, String followers,
+                          String age_name, int kind) {
+        this.id_funny = id_funny;
+        this.id_channel = id_channel;
+        this.id_reality = id_reality;
+        this.id_study = id_study;
+        this.id_farsi = id_farsi;
+        this.view = view;
+        this.liky = liky;
+        this.title_en = title_en;
+        this.title_fa = title_fa;
+        this.poster = poster;
+        this.link_480 = link_480;
+        this.link_720 = link_720;
+        this.profile_chann = profile_chann;
+        this.banner_chann = banner_chann;
+        this.followers = followers;
+        this.age_name = age_name;
+        this.kind = kind;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -137,6 +160,9 @@ public class FunnyDataModel implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeInt(id_funny);
         dest.writeInt(id_channel);
+        dest.writeInt(id_reality);
+        dest.writeInt(id_study);
+        dest.writeInt(id_farsi);
         dest.writeString(view);
         dest.writeString(liky);
         dest.writeString(title_en);
@@ -144,10 +170,10 @@ public class FunnyDataModel implements Parcelable {
         dest.writeString(poster);
         dest.writeString(link_480);
         dest.writeString(link_720);
-        dest.writeString(name_chan_en);
-        dest.writeString(name_chan_fa);
         dest.writeString(profile_chann);
         dest.writeString(banner_chann);
         dest.writeString(followers);
+        dest.writeString(age_name);
+        dest.writeInt(kind);
     }
 }
